a00dc9ba09ea1ce44b2b87647f82a40629a331c1
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-03-25 20:00:08 +0200

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const categoriesEl = document.querySelectorAll(".item");
+
+console.log("Number of categories:", categoriesEl.length);
+
+categoriesEl.forEach((category) => {
+    console.log("Category:", category.querySelector("h2").textContent);
+    console.log("Elements:", category.querySelectorAll("li").length);
+}
+    
+    
+);
